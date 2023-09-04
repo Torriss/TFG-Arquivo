7edf05d1c8cbaf3621c961bb15c2de8c0602c7da
TORRIS-PC\diego <diegogarciatorralba.98@gmail.com>
2023-09-04 19:47:31 +0200

    transferencia finalizada

--- src/DAO/TransferenciaDAO.java
+++ src/DAO/TransferenciaDAO.java
@@ -1,9 +1,11 @@
 package DAO;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 
 import model.Expediente;
 
 public interface TransferenciaDAO {
-	public List<Expediente> transferirExpedientes(String filePath);
+	public List<Expediente> transferirExpedientes(String filePath) throws ClassNotFoundException, SQLException, IOException;
 }
--- src/DAO/TransferenciaDAOImpl.java
+++ src/DAO/TransferenciaDAOImpl.java
@@ -2,8 +2,8 @@ package DAO;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -13,14 +13,16 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import DAO.*;
 import model.Caja;
 import model.Expediente;
 
 public class TransferenciaDAOImpl implements TransferenciaDAO {
 
-    public List<Expediente> transferirExpedientes(String filePath) {
-        List<Expediente> expedientes = new ArrayList<>();
+    public List<Expediente> transferirExpedientes(String filePath) throws ClassNotFoundException, SQLException, IOException {
+        List<Expediente> expedientesNuevos = new ArrayList<>();
+        List<Expediente> expedientesActualizar = new ArrayList<>();
+        CajaDAO cajas = new CajaDAOImpl();
+        ExpedienteDAO exp = new ExpedienteDAOImpl();
 
         try {
             FileInputStream fis = new FileInputStream(new File(filePath));
@@ -40,36 +42,55 @@ public class TransferenciaDAOImpl implements TransferenciaDAO {
                 Row row = rowIterator.next();
                 Iterator<Cell> cellIterator = row.cellIterator();
 
-                // Crear un objeto Expediente y asignar valores desde el archivo Excel
-//                Expediente expediente = new Expediente();
-//
-//                expediente.setNumExpediente((int) cellIterator.next().getNumericCellValue());
-//                expediente.setTipo(cellIterator.next().getStringCellValue());
-//                expediente.setAnio((int) cellIterator.next().getNumericCellValue());
-//                // ... asignar otros atributos de expediente
-//                
-//                CajaDAO cajas = new CajaDAOImpl();
-//                List<Caja> caja = cajas.buscarCajasParaExpedienteNuevo(expediente.getAnio(), expediente.getTipo(), expediente.getPaginas());
-//                //actualizar ubi de expediente
-//                //actualizar caja de expediente
-//                //restar paginas de caja
-//                
-//                expedientes.add(expediente);
+                //Crear un objeto Expediente y asignar valores desde el archivo Excel
+                Expediente expediente = new Expediente();
+
+                expediente.setTipo(cellIterator.next().getStringCellValue());
+                expediente.setNumExpediente((int) cellIterator.next().getNumericCellValue());
+                expediente.setAnio((int) cellIterator.next().getNumericCellValue());
+                expediente.setUbicacion(cellIterator.next().getStringCellValue());
+                expediente.setNotas(cellIterator.next().getStringCellValue());
+                expediente.setTomos(cellIterator.next().getStringCellValue());
+                expediente.setJuzgado(cellIterator.next().getStringCellValue());
+                expediente.setLugar(cellIterator.next().getStringCellValue());
+                expediente.setCaja((int) cellIterator.next().getNumericCellValue());
+                expediente.setPaginas((int) cellIterator.next().getNumericCellValue());
+                expediente.setEstado(cellIterator.next().getStringCellValue());
+                
+                //Expediente existe en bbdd por lo que solo actualizamos
+                if(exp.existeExpediente(expediente.getNumExpediente(), expediente.getTipo(), expediente.getAnio(), expediente.getJuzgado())) {
+                	expedientesActualizar.add(expediente);
+                }
+                else expedientesNuevos.add(expediente);
             }
 
             fis.close();
             workbook.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            throw e;
         }
 
-        // Llamar a la funci�n buscarNuevaCaja para actualizar la ubicaci�n de los expedientes
-        ExpedienteDAO expedienteDAO = new ExpedienteDAOImpl();
-        for (Expediente expediente : expedientes) {
-//            expedienteDAO.insert(expediente);
+        for (Expediente expediente : expedientesNuevos) {
+        	//buscamos caja nueva para expediente
+        	Caja caja = cajas.buscarCajasParaExpedienteNuevo(expediente.getAnio(), expediente.getTipo(), expediente.getPaginas());
+        	//actualizar ubi de expediente
+        	expediente.setUbicacion(caja.getUbicacion());
+        	//actualizar caja de expediente
+        	expediente.setCaja(caja.getIdCaja());
+        	//restar paginas de caja
+        	caja.restarPaginas(expediente.getPaginas());
+        	//hacemos update e insert en bbdd
+        	cajas.update(caja);
+        	exp.insert(expediente);
         }
-
-        return expedientes;
+        for (Expediente expediente : expedientesActualizar) {
+        	exp.update(expediente);
+        }
+        
+        //juntamos listas para retorno
+        expedientesNuevos.addAll(expedientesActualizar);
+        
+        return expedientesNuevos;
     }
 }
 
