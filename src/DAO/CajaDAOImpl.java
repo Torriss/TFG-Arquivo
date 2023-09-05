9fc25ea0a81c39ce2e877ae7358c35e7c7a7ae85
TORRIS-PC\diego <diegogarciatorralba.98@gmail.com>
2023-09-05 15:15:09 +0200

    Cambios cajaDAOImpl

--- src/DAO/CajaDAOImpl.java
+++ src/DAO/CajaDAOImpl.java
@@ -3,6 +3,7 @@ package DAO;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+
 import model.Caja;
 import model.Conexion;
 
@@ -76,17 +77,57 @@ public class CajaDAOImpl implements CajaDAO {
     
     public Caja buscarCajasParaExpedienteNuevo(int anio, String tipo, int paginasExpediente) throws SQLException, ClassNotFoundException{
         ArrayList<Caja> cajasMismoTipoYAnio = obtenerCajasPorTipoYAnio(tipo, anio);
-
-        for (Caja caja : cajasMismoTipoYAnio) {
-            if (caja.getPaginas() >= paginasExpediente) return caja;
+        if(!cajasMismoTipoYAnio.isEmpty()) {
+        	for (Caja caja : cajasMismoTipoYAnio) {
+                if (caja.getPaginas() >= paginasExpediente) return caja;
+            }
+        	String ultimaUbicacion = cajasMismoTipoYAnio.get(cajasMismoTipoYAnio.size() - 1).getUbicacion();
+            String nuevaUbicacion = buscarNuevaUbicacionContigua(ultimaUbicacion);
+            Caja nuevaCaja = new Caja(nuevaUbicacion, tipo, anio);
+            this.insert(nuevaCaja);
+            return nuevaCaja;
         }
-
-        String ultimaUbicacion = cajasMismoTipoYAnio.get(cajasMismoTipoYAnio.size() - 1).getUbicacion();
-        String nuevaUbicacion = buscarNuevaUbicacionContigua(ultimaUbicacion);
-        Caja nuevaCaja = new Caja(nuevaUbicacion, tipo, anio);
-        this.insert(nuevaCaja);
+        else {
+        	ArrayList<Caja> cajasMismoTipo = obtenerCajasPorTipo(tipo);
+        	if(!cajasMismoTipo.isEmpty()) {
+            	for (Caja caja : cajasMismoTipo) {
+                    if (caja.getPaginas() >= paginasExpediente) return caja;
+                }
+            	String ultimaUbicacion = cajasMismoTipo.get(cajasMismoTipo.size() - 1).getUbicacion();
+                String nuevaUbicacion = buscarNuevaUbicacionContigua(ultimaUbicacion);
+                Caja nuevaCaja = new Caja(nuevaUbicacion, tipo, anio);
+                this.insert(nuevaCaja);
+                return nuevaCaja;
+            }
+            else {
+            	ArrayList<Caja> cajasMismoAnio = obtenerCajasPorAnio(anio);
+                if(!cajasMismoAnio.isEmpty()) {
+                	for (Caja caja : cajasMismoAnio) {
+                        if (caja.getPaginas() >= paginasExpediente) return caja;
+                    }
+                	String ultimaUbicacion = cajasMismoAnio.get(cajasMismoAnio.size() - 1).getUbicacion();
+                    String nuevaUbicacion = buscarNuevaUbicacionContigua(ultimaUbicacion);
+                    Caja nuevaCaja = new Caja(nuevaUbicacion, tipo, anio);
+                    this.insert(nuevaCaja);
+                    return nuevaCaja;
+                }
+            }
+        }
+        
+        String ultimaUbi = ultimaUbicacion();
+        Caja nuevaCaja = new Caja(buscarNuevaUbicacionContigua(ultimaUbi), tipo, anio);
         
         return nuevaCaja;
+    }
+    
+    private String ultimaUbicacion() throws SQLException, ClassNotFoundException {
+    	String query = "SELECT * FROM cajas ORDER BY ubicacion asc";
+        ResultSet rs = Conexion.executeSelect(query);
+        String ult = "";
+        
+        if (rs.next()) ult = rs.getString("ubicacion");
+            
+        return ult;
     }
 
     private ArrayList<Caja> obtenerCajasPorTipoYAnio(String tipo, int anio) throws SQLException, ClassNotFoundException{
@@ -104,6 +145,42 @@ public class CajaDAOImpl implements CajaDAO {
         }
         
         return cajasMismoTipoYAnio;
+    }
+    
+    private ArrayList<Caja> obtenerCajasPorTipo(String tipo) throws SQLException, ClassNotFoundException{
+        ArrayList<Caja> cajasMismoTipo = new ArrayList<>();
+        String query = "SELECT * FROM cajas WHERE tipo = '" + tipo + "'";
+        ResultSet rs = Conexion.executeSelect(query);
+        
+        
+        while (rs.next()) {
+        	int idCaja = rs.getInt("idCaja");
+            int paginas = rs.getInt("paginas");
+            String ubicacion = rs.getString("ubicacion");
+            int anio = rs.getInt("anio");
+            Caja caja = new Caja(idCaja, paginas, ubicacion, tipo, anio);
+            cajasMismoTipo.add(caja);
+        }
+        
+        return cajasMismoTipo;
+    }
+    
+    private ArrayList<Caja> obtenerCajasPorAnio(int anio) throws SQLException, ClassNotFoundException{
+        ArrayList<Caja> cajasMismoAnio = new ArrayList<>();
+        String query = "SELECT * FROM cajas WHERE anio = " + anio; 
+        ResultSet rs = Conexion.executeSelect(query);
+        
+        
+        while (rs.next()) {
+        	int idCaja = rs.getInt("idCaja");
+            int paginas = rs.getInt("paginas");
+            String ubicacion = rs.getString("ubicacion");
+            String tipo = rs.getString("tipo");
+            Caja caja = new Caja(idCaja, paginas, ubicacion, tipo, anio);
+            cajasMismoAnio.add(caja);
+        }
+        
+        return cajasMismoAnio;
     }
     
     private String buscarNuevaUbicacionContigua(String ultimaUbicacion) {
